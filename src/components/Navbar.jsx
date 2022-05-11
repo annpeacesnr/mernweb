1c88b90f5be9d9d5c9e10bc1f58b2c0ffc96e7db
Anna <am.spencer@live.co.uk>
2022-05-11 18:13:00 +0100

    import navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <div><nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
+    <div><nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
       <a class="navbar-brand" href="#">Navbar</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -40,3 +40,5 @@ export default function Navbar() {
   </nav></div>
   )
 }
+
+export default Navbar;
