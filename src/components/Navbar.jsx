17eca369f363ebe88ffbd5284d52154f50d10f09
Anna <am.spencer@live.co.uk>
2022-05-11 18:28:16 +0100

    added button icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,8 +24,11 @@ const Navbar = () => {
           </li>
         </ul>
         <a class="navbar-brand fw-bolder fs-4 mx-auto" href="#">Navbar</a>
-        <button className="btn btn-outline-primary ms-auto px-4 rounded-pill">Login</button>
-        <button className="btn btn-outline-primary ms-auto px-4 rounded-pill">Register</button>
+        <button className="btn btn-outline-primary ms-auto px-4 rounded-pill">
+        <i className="fa fa-sign-in me-2"></i>Login</button>
+
+        <button className="btn btn-outline-primary ms-2 px-4 rounded-pill">
+        <i className="fa fa-user-plus me-2"></i>Register</button>
       </div>
     </div>
   </nav></div>
