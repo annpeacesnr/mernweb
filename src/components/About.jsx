c301d1f1e84f538b604f852141ee7c96e38a2986
Anna <am.spencer@live.co.uk>
2022-05-11 19:20:41 +0100

    added image and changed background colour

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <section className="about">
+            <div className="container my-5 py-5">
+                <div className="row">
+                    <div className="col-md-6">
+                        <img src="/assets/about1.jpg" alt="About" className="w-75 mt-5" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+  );
+}
+
+export default About;
