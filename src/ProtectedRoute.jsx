a4ff8119da47171c1207286811ec808360ca6f39
Anna <am.spencer@live.co.uk>
2022-05-15 03:23:45 +0100

    imported navigate

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router';
+import { Navigate, Route } from 'react-router';
 
 const Protectedroute = ({auth, component:Component, ...rest}) => {
     return (
         <div>
             <Route {...rest} render={(props)=>{
                 if(auth) return <Component {...props} />
-                if(!auth) return <Redirect to={{path : '/', state : {from : props.location}}} />
+                if(!auth) return <Navigate to={{path : '/', state : {from : props.location}}} />
             }} />
         </div>
     );
